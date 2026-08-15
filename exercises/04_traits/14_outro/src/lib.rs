0a2053816e2fe28e6b7f0d853e56a1016d2738a5
Chris Cody <3807221+codeitlikecody@users.noreply.github.com>
2026-08-15 12:33:53 +1000

    exercises/04_traits/14_outro

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -1,10 +1,89 @@
 // TODO: Define a new `SaturatingU16` type.
 //   It should hold a `u16` value.
-//   It should provide conversions from `u16`, `u8`, `&u16` and `&u8`.
-//   It should support addition with a right-hand side of type
-//   SaturatingU16, u16, &u16, and &SaturatingU16. Addition should saturate at the
-//   maximum value for `u16`.
-//   It should be possible to compare it with another `SaturatingU16` or a `u16`.
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+impl SaturatingU16 {
+    pub fn new(value: u16) -> Self {
+        SaturatingU16 { value }
+    }
+}
+
+//   It should be possible to compare it with another `SaturatingU16` or a `u16`.
+impl ::core::cmp::PartialEq<u16> for SaturatingU16 {
+    #[inline]
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
+
+//   It should provide conversions from `u16`, `u8`, `&u16` and `&u8`.
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        SaturatingU16 { value }
+    }
+}
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        SaturatingU16 { value: *value }
+    }
+}
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        SaturatingU16 {
+            value: value.into(),
+        }
+    }
+}
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        SaturatingU16 {
+            value: u8::into(*value),
+        }
+    }
+}
+
+//   It should support addition with a right-hand side of type
+//   SaturatingU16, u16, &u16, and &SaturatingU16. Addition should saturate at the
+//   maximum value for `u16`.
+impl std::ops::Add for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs.value),
+        }
+    }
+}
+impl std::ops::Add<&SaturatingU16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &Self) -> Self {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs.value),
+        }
+    }
+}
+impl std::ops::Add<u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: u16) -> Self {
+        SaturatingU16 {
+            value: self.value.saturating_add(rhs),
+        }
+    }
+}
+impl std::ops::Add<&u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &u16) -> Self {
+        SaturatingU16 {
+            value: self.value.saturating_add(*rhs),
+        }
+    }
+}
