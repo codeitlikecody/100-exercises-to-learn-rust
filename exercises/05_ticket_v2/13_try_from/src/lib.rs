a178902a53dfad461c03caee39acaa4cd29182f7
Chris Cody <3807221+codeitlikecody@users.noreply.github.com>
2026-08-18 00:47:50 +1000

    exercises/05_ticket_v2/13_try_from

--- exercises/05_ticket_v2/13_try_from/src/lib.rs
+++ exercises/05_ticket_v2/13_try_from/src/lib.rs
@@ -7,6 +7,28 @@ enum Status {
     InProgress,
     Done,
 }
+impl TryFrom<String> for Status {
+    type Error = String;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match &*value.to_lowercase() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err("()".into()),
+        }
+    }
+}
+impl TryFrom<&str> for Status {
+    type Error = String;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        match &*value.to_lowercase() {
+            "todo" => Ok(Status::ToDo),
+            "inprogress" => Ok(Status::InProgress),
+            "done" => Ok(Status::Done),
+            _ => Err("()".into()),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
