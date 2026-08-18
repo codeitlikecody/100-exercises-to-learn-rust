3719f4621c79123ce4c76b47f1cdc47227cdc7ee
Chris Cody <3807221+codeitlikecody@users.noreply.github.com>
2026-08-19 00:16:39 +1000

    exercises/05_ticket_v2/14_source

--- exercises/05_ticket_v2/14_source/src/lib.rs
+++ exercises/05_ticket_v2/14_source/src/lib.rs
@@ -23,6 +23,11 @@ pub enum TicketNewError {
     DescriptionCannotBeEmpty,
     #[error("Description cannot be longer than 500 bytes")]
     DescriptionTooLong,
+    #[error("`invalid` is not a valid status. Use one of: ToDo, InProgress, Done")]
+    ParseStatusError {
+        #[from]
+        inner: crate::status::ParseStatusError,
+    },
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -48,7 +53,7 @@ impl Ticket {
         }
 
         // TODO: Parse the status string into a `Status` enum.
-
+        let status = status.try_into()?;
         Ok(Ticket {
             title,
             description,
